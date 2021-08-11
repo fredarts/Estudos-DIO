dc8eab6a634d4b1bb4daec1fd3ff184295935894
fredarts <fredericomonteiromendesmaia@gmail.com>
2021-08-11 15:21:52 -0700

    Jogo da cobrinha

---
+++ Programação para internet com JavaScript/js/main.js
@@ -0,0 +1,117 @@
+
+
+
+
+
+
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+    
+
+
+}
+
+function redirecionar(){
+    window.open("https://www.youtube.com/");
+
+}
+
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Novo Texto";
+   
+
+
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+   
+
+
+}
+
+
+
+/*
+
+function validaIdade(){
+    var validar;
+    if(idade >= 18){
+        validar = true
+    }
+    else {
+        validar = false
+    }
+
+    return validar
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+
+function soma(sn1, sn2){
+    return sn1 + sn2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+
+var d = new Date;
+console.log(d.getMonth());
+
+var count;
+for (count = 0; count < 5; count++){
+    console.log(count)
+    
+
+};
+
+
+
+var idade = 0;
+while (idade < 5){
+    alert(idade);
+    idade++;
+
+};
+
+
+var idade = prompt("Qual a sua idade");
+if (idade >= 18){
+    console.log("Maior de Idade");
+}
+    else console.log("Menor de Idade");
+
+
+var nome = "Frederico Monteiro";
+var idade = 33;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade);
+console.log(frase.toUpperCase());
+
+
+var lista = ["Maça", "Pera","Uva"];
+
+console.log(lista.toString());
+
+
+var frutas = {nome: "Uva", Cor: "Roxa"};
+console.log(frutas.Cor);
+
+
+var frutas = [{nome: "Uva", Cor: "Roxa"},{nome: "Maçã", Cor: "Vermelha"}];
+console.log(frutas[1].Cor);*/
